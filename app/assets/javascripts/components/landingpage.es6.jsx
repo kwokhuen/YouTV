a686825c5116873a07caa7a2ddf1a887b79fb921
kwokhuen <benjaminkwokhuen@gmail.com>
2016-12-20 17:20:23 -0800

    add about us in landing page

--- app/assets/javascripts/components/landingpage.es6.jsx
+++ app/assets/javascripts/components/landingpage.es6.jsx
@@ -6,15 +6,23 @@ class LandingPage extends React.Component {
 
   render(){
     return(
-      <div className="wrapper">
-        <div>
-          <h1 className="slogan">Click, play, enjoy.</h1>
-          <p className="long-slogan">DON'T WASTE YOUR TIME PICKING VIDEO. LET US DO THE REST.</p>
-        </div>
-        <div>
-          <ModalRegister className="landingpage-modal"
-            handleRegister={this.handleRegister}/>
-        </div>
+      <div className="outer-wrapper">
+
+        <div className="wrapper">
+          <div>
+            <h1 className="slogan">Click, play, enjoy.</h1>
+            <p className="long-slogan">DON'T WASTE YOUR TIME PICKING VIDEO. LET US DO THE REST.</p>
+          </div>
+          <div>
+            <ModalRegister className="landingpage-modal"
+              handleRegister={this.handleRegister}/>
+            </div>
+          </div>
+
+          <div className="about-us">
+            <img src="about-us.png" />
+            <p className="about-us-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
       </div>
     );
   }
