24bd6b8b2ffa75b787dc835bba509ddd942f73d2
Benjamin Kwok <email": null>
2016-12-21 19:32:14 -0800

    add styling

--- app/assets/javascripts/components/slicker.es6.jsx
+++ app/assets/javascripts/components/slicker.es6.jsx
@@ -2,11 +2,15 @@ class SimpleSlider extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state={}
-
+    this.state={
+      isGrid: false
+    }
+    this.toggle = this.toggle.bind(this);
   }
 
-
+  toggle() {
+    this.setState({isGrid: !this.state.isGrid})
+  }
 
   render() {
     const slickerStyle = {
@@ -16,19 +20,45 @@ class SimpleSlider extends React.Component {
     // Variable Width
     var settings = {
       slidesToShow: 3,
-      centerMode: true,
       variableWidth: true
     };
-
-    return (
-      <Slider {...settings}>
-        <div><img onClick={this.props.handleClick} id="1" style={slickerStyle} src="comedy.png"/><h4 className="category_title">Comedy Network</h4></div>
-        <div><img onClick={this.props.handleClick} id="2" style={slickerStyle} src="education.png"/><h4 className="category_title">Educational & Informative</h4></div>
-        <div><img onClick={this.props.handleClick} id="3" style={slickerStyle} src="kids.png"/><h4 className="category_title">Kids Discovery</h4></div>
-        <div><img onClick={this.props.handleClick} id="4" style={slickerStyle} src="cook.png"/><h4 className="category_title">Cooking</h4></div>
-        <div><img onClick={this.props.handleClick} id="5" style={slickerStyle} src="entertain.png"/><h4 className="category_title">Enterainment</h4></div>
-        <div><img onClick={this.props.handleClick} id="6" style={slickerStyle} src="game.png"/><h4 className="category_title">Gamer</h4></div>
-      </Slider>
-    );
+    var Button = ReactBootstrap.Button;
+    if (this.state.isGrid) {
+      return (
+        <div className="grid-container">
+          <div className="display-button">
+            <Button bsStyle="info" onClick={this.toggle}>Slider</Button>
+          </div>
+          <div className="categories-container">
+              <div className="blah">
+                <div className="category-container"><div className="category-image"><img className="center-block" class="center-block" onClick={this.props.handleClick} id="1" style={slickerStyle} src="comedy.png"/></div><h4 className="category_title center">Comedy Network</h4></div>
+                <div className="category-container"><div className="category-image"><img className="center-block" class="center-block" onClick={this.props.handleClick} id="2" style={slickerStyle} src="education.png"/></div><h4 className="category_title center">Educational & Informative</h4></div>
+                <div className="category-container"><div className="category-image"><img className="center-block" class="center-block" onClick={this.props.handleClick} id="3" style={slickerStyle} src="kids.png"/></div><h4 className="category_title center">Kids Discovery</h4></div>
+              </div>
+              <div className="blah">
+                <div className="category-container"><div className="category-image"><img className="center-block" class="center-block" onClick={this.props.handleClick} id="4" style={slickerStyle} src="cook.png"/></div><h4 className="category_title center">Cooking</h4></div>
+                <div className="category-container"><div className="category-image"><img className="center-block" class="center-block" onClick={this.props.handleClick} id="5" style={slickerStyle} src="entertain.png"/></div><h4 className="category_title center">Enterainment</h4></div>
+                <div className="category-container"><div className="category-image"><img className="center-block" class="center-block" onClick={this.props.handleClick} id="6" style={slickerStyle} src="game.png"/></div><h4 className="category_title center">Gamer</h4></div>
+              </div>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="grid">
+          <div className="display-button">
+            <Button bsStyle="info" onClick={this.toggle}>Grid</Button>
+          </div>
+          <Slider className={this.state.slider} {...settings}>
+            <div><img onClick={this.props.handleClick} id="1" style={slickerStyle} src="comedy.png"/><h4 className="category_title">Comedy Network</h4></div>
+            <div><img onClick={this.props.handleClick} id="2" style={slickerStyle} src="education.png"/><h4 className="category_title">Educational & Informative</h4></div>
+            <div><img onClick={this.props.handleClick} id="3" style={slickerStyle} src="kids.png"/><h4 className="category_title">Kids Discovery</h4></div>
+            <div><img onClick={this.props.handleClick} id="4" style={slickerStyle} src="cook.png"/><h4 className="category_title">Cooking</h4></div>
+            <div><img onClick={this.props.handleClick} id="5" style={slickerStyle} src="entertain.png"/><h4 className="category_title">Enterainment</h4></div>
+            <div><img onClick={this.props.handleClick} id="6" style={slickerStyle} src="game.png"/><h4 className="category_title">Gamer</h4></div>
+          </Slider>
+        </div>
+      );
+    }
   }
 };
